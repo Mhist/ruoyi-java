d4174e92db4f8a855dd73fb1e7e36f5dfb208629
jiang yadong <18827637106@163.com>
2023-03-27 16:23:48 +0800

    fix:若依后台基础环境

---
+++ ruoyi-ui/src/views/test/editor/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div class="qualityManual-container">
+      <div>
+        <button style="width: 120px;" type="primary" @click="getFile">测试预览office文档</button>
+        <button style="width: 120px;" type="primary" @click="close">关闭</button>
+      </div>
+      <div v-if="show" class="qualityManual-container-office">
+        <vab-only-office :option="option" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import vabOnlyOffice from '../../../components/OnlyOffice/index.vue'
+
+export default {
+  name: '',
+  components: {
+    vabOnlyOffice
+  },
+  data() {
+    return {
+      // 参考vabOnlyOffice组件参数配置
+      option: {
+        url: '',
+        isEdit: '',
+        fileType: '',
+        title: '',
+        lang: '',
+        isPrint: '',
+        user: { id: null, name: '' }
+      },
+      show: false
+    }
+  },
+  methods: {
+    getFile() {
+      this.show = true
+      // getAction('/file/selectById', { id: this.id }).then(res => {
+      this.option.isEdit = true
+      this.option.lang = 'zh-CN'
+      this.option.url = 'http://119.91.213.59:8888/down/RDUwFJkecgc7.docx'
+      this.option.title = '123'
+      this.option.fileType = 'docx'
+      this.option.isPrint = false
+      this.option.user = { id: 12, name: '江亚东' }
+      this.option.token = 'eGw306kukzJDM4FeL05Z'
+      // })
+    },
+    close() {
+      this.show = false
+    }
+  }
+}
+</script>
+
+<style>
+.qualityManual-container-office {
+  height: 200px;
+}
+</style>
---
+++ ruoyi-ui/src/views/test/tableform/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <el-button type="text" @click="dialogVisible = true"
+      >点击打开 Dialog</el-button
+    >
+    <el-button type="text" @click="dialogVisible = true"
+      >点击打开 Dialog</el-button
+    >
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="50%"
+    >
+    <el-form ref="form" :model="form">
+      <el-table  ref="table" :data="form.tableData" empty-text='暂无数据'>
+        <el-table-column label="编号" type="index" width="80">
+       
+        </el-table-column>
+        <el-table-column label="子域名称" width="auto">
+          <template slot-scope="scope">
+            <el-form-item >
+              <el-input v-model="scope.row.name"></el-input>
+            </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column label="子域描述" width="auto">
+          <template slot-scope="scope">
+            <el-form-item >
+              <el-input v-model="scope.row.description"></el-input>
+            </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column  label="操作" width="180">
+          <template slot-scope="scope">
+            <el-button type="danger" size="mini" @click="del(scope.$index)" v-show="length>1" icon='el-icon-delete'></el-button>
+            <el-button type="primary" size="mini" @click="add" icon='el-icon-plus'></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  created(){
+  },
+  computed:{
+    length: function () {
+    return this.form.tableData.length;
+  }
+  },
+  data() {
+    return {
+        dialogVisible: false,
+        form:{
+            tableData:[
+                {
+            name: "",
+            description: ""
+          },
+            ]
+        }
+    };
+  },
+  methods: {
+
+    add(){
+          this.form.tableData.push({
+              name: "",
+              description: ''
+          })
+      },
+      del(index){
+          this.form.tableData.splice(index,1);
+      }
+  },
+};
+</script>
+
+<style></style>
